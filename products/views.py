b935dc5205fb5cd665f761239b4780d3d5256ff1
Rafat Ramoni <rafatramoni15@gmail.com>
2024-05-27 09:43:33 +0000

    Edited ratings of products

--- products/views.py
+++ products/views.py
@@ -9,12 +9,25 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
-    categories = Category.objects.all()
-
     query = None
-    category = None
+    categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+        if 'direction' in request.GET:
+            direction = request.GET['direction']
+            if direction == 'desc':
+                sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
